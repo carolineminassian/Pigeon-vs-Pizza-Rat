dc363a0299bbb2fa7d321153b85290694a36f4a8
Caroline Minassian <caroline.m.minassian@gmail.com>
2021-07-14 01:18:30 +0200

    pizza gravity

--- pigeon.js
+++ pigeon.js
@@ -1,23 +1,21 @@
 class Pigeon extends Player {
-    constructor(game, y) {
-        super(game, 0, y);
-        this.width = 20;
-        this.height = 20;
-        this.enableControls();
-    }
+  constructor(game, y) {
+    super(game, 0, y);
+    this.width = 20;
+    this.height = 20;
+    // this.enableControls();
+  }
 
-    enableControls() {
-        window.addEventListener('mousemove', (event) => {
-            
+  // enableControls() {
+  //     window.addEventListener('mousemove', (event) => {
 
-    }
+  // }
 
-    paint() {
-        const context = this.game.context;
-        context.save();
-        context.fillStyle = 'blue';
-        context.fillRect(this.x, this.y, this.width, this.height);
-        context.restore();
-    }
-
+  paint() {
+    const context = this.game.context;
+    context.save();
+    context.fillStyle = 'blue';
+    context.fillRect(this.x, this.y, this.width, this.height);
+    context.restore();
+  }
 }
--- pizza.js
+++ pizza.js
@@ -1,25 +1,28 @@
+const GRAVITY = 0.2;
+
 class Pizza {
-    constructor(game, x, y) {
-        this.game = game;
-        this.x = x;
-        this.y = y;
-        this.width = 20;
-        this.height = 30;
+  constructor(game, x, y) {
+    this.game = game;
+    this.x = x;
+    this.y = y;
+    this.width = 20;
+    this.height = 30;
+    this.speedY = 0;
+    this.speedX = 2;
+  }
+
+  runLogic() {
+    if (this.y < this.game.canvas.height - this.height) {
+      this.speedY += GRAVITY;
+      this.y += this.speedY;
     }
+  }
 
-    runLogic() {
-        if (this.y < this.game.canvas.height-this.height) {
-            this.y++;
-        }
-    }
-
-    paint() {
-        const context = this.game.context;
-        context.save();
-        context.fillStyle = 'orange';
-        context.fillRect(this.x, this.y, this.width, this.height);
-        context.restore();
-    }
-
-
+  paint() {
+    const context = this.game.context;
+    context.save();
+    context.fillStyle = 'orange';
+    context.fillRect(this.x, this.y, this.width, this.height);
+    context.restore();
+  }
 }
